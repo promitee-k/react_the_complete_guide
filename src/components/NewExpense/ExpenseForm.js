d56e24ba6659158d813fe0328330167271001556
Nusrat Tahsin Kamaly <nusrat.tahsin@thesaurus.co.jp>
2023-03-02 14:09:48 +0900

    upto expense form using useState

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { useState } from "react";
+const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div>
+        <label>Title</label>
+        <input
+          type="text"
+          value={enteredTitle}
+          onChange={titleChangeHandler}
+        ></input>
+        <label>Amount</label>
+        <input
+          type="number"
+          min="0.1"
+          step="0.01"
+          value={enteredAmount}
+          onChange={amountChangeHandler}
+        ></input>
+        <label>Date</label>
+        <input
+          type="date"
+          min="2023-01-01"
+          max="2024-12-31"
+          value={enteredDate}
+          onChange={dateChangeHandler}
+        ></input>
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
